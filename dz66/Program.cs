fbc33c08ea4cd731e037c0caf479f8c261ec85d6
Ildar <ildar.azmatov@ya.ru>
2022-12-26 01:42:30 +0500

    сумму натуральных элементов в промежутке от M до N

---
+++ dz66/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Задайте значение m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение n");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+if (m > n) { (m, n) = (n, m); }
+for (int i=m; i<=n; i++)
+sum = sum + i;
+Console.Write($"{m}, {n} -> {sum}");
